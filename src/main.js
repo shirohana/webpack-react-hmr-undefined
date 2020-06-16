0d55ee38a582ecc41b80997cddf47fe7ae728d1c
Hana Shiro <shirohana0608@gmail.com>
2020-06-16 11:21:18 +0800

    Fix react-hot-loader disposing it self in main.js (no affect to current issue)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 // @flow
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
+import App from './app/index'
 
 const APP_ROOT_ID = 'app-root'
 const APP_ROOT = document.getElementById(APP_ROOT_ID)
 
-import('./app/index').then(({ default: App }) => {
-  const app = React.createElement(App)
-  render(app, APP_ROOT)
-})
+function render (Component) {
+  ReactDOM.render(
+    React.createElement(Component),
+    APP_ROOT
+  )
+}
+
+render(App)
